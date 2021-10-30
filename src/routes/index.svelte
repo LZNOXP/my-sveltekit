56f3b35244aeb029d7e2981b2f7d16ae46664e5d
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2021-10-30 15:34:28 +0800

    tailwind installed

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let value: string = '';
+	console.log('hello');
+</script>
+
+<svelte:head>
+	<title>Template</title>
+</svelte:head>
+
+<h1 class="bg-green-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<input class="border-green-800 border" type="text" bind:value />
+<button class="border border-blue-500 p-4" on:click={() => goto(`/hello/${value}`)}>
+	Say hello
+</button>
